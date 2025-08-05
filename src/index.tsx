b39cf384a202ab5786989505e6acf6cbc5f063be
zhizhonglin <lynzz168@gmail.com>
2025-08-05 23:51:13 +0800

    chore: Remove cost-benefit analysis section from slides; integrate UserProfile component into the main application rendering

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { TodoApp } from './components/TodoApp';
 import './index.css';
+import { UserProfile } from './components/UserProfile';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <TodoApp />
+    <UserProfile/>
   </React.StrictMode>
 ); 
